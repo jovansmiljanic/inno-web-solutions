f19826284a7d09fba3965fefd9a8a33f665a56bc
Jovan Smiljanic <smiljanicjovan9@gmail.com>
2024-01-14 16:09:51 +0100

    Meta description added

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -19,6 +19,12 @@ const poppins = Poppins({
   weight: ["400", "500", "600", "700"],
 });
 
+export const metadata = {
+  title: "Inno Web Solutions",
+  description:
+    "Inno Web Solutions is a digital agency specializing in innovative web and app development, with a strong focus on SEO and custom design solutions. Their services range from frontend and backend development to mobile app creation and e-commerce platforms. They emphasize personalized approaches to digital strategy, aiming to enhance client satisfaction and achieve significant revenue growth through their tailored solutions.",
+};
+
 export default function RootLayout({
   children,
   params,
